e658d60c630169a3cbed6790a4e5f437b4689cc7
张双意 <zhangdan@zhangdandeMacBook-Air.local>
2023-01-09 11:39:45 +0800

    feat: 新建Action组件基础目录

---
+++ packages/vue-pro-components/src/Action/index.ts
@@ -0,0 +1,8 @@
+import { withInstall } from '@vue-pro-components/utils';
+import ActionDialogComp from './ActionDialog';
+
+export const ActionDialog = withInstall(ActionDialogComp);
+
+export default {
+  ActionDialog,
+};
