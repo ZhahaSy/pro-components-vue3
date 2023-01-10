5d166b781149b429ede0ba954f3c19cb51be497d
shirui01 <shirui01@bilibili.com>
2023-01-10 16:25:10 +0800

    feat: add basic button component

--- packages/vue-pro-components/src/Action/ActionDialog.tsx
+++ packages/vue-pro-components/src/Action/ActionDialog.tsx
@@ -1,3 +1,23 @@
-export default {
-  name: 'ActionDialog',
-};
+import { defineComponent, ref } from 'vue';
+import './style.css';
+import Modal from '../Modal/BasicModal';
+
+interface ButtonProps {
+  type: 'primary' | 'dashed' | 'link';
+}
+
+const Button = defineComponent({
+  setup(props: ButtonProps) {
+    const visible = ref<boolean>(false);
+    const showModal = () => {
+      visible.value = true;
+    };
+    return () => (
+      <a-button class={['btn', `btn-${props.type}`]} showModal={showModal}>
+        <Modal v-model:visible={visible}></Modal>
+      </a-button>
+    );
+  },
+});
+
+export default Button;
