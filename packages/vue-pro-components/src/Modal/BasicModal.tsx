f68cd0142f35526fccd93afaacc6f38a629fe630
shirui01 <shirui01@bilibili.com>
2023-01-10 16:26:16 +0800

    feat: add basic modal component

---
+++ packages/vue-pro-components/src/Modal/BasicModal.tsx
@@ -0,0 +1,61 @@
+import { CSSProperties, defineComponent, computed } from 'vue';
+import PrefixTitle from '../Prefix/PrefixTitle';
+
+enum WidthType {
+  string,
+  number,
+}
+interface Props {
+  visible: boolean;
+  title: { type: string; default: '' };
+  bodyStyle: { type: CSSProperties; default: undefined };
+  width: { type: WidthType; default: '' };
+  titleProps: { type: Record<string, unknown>; default: undefined };
+}
+
+export type ModalProps = Partial<Props>;
+
+export default defineComponent({
+  setup(props: ModalProps, { slots, attrs }) {
+    const computedWidth = computed(() => {
+      const type = typeof props.width;
+      if (type === 'number') {
+        return `${props.width}px`;
+      }
+      return props.width;
+    });
+
+    const handleOk = (e: MouseEvent) => {
+      console.log(e);
+      // eslint-disable-next-line vue/no-mutating-props
+      props.visible = false;
+    };
+
+    return () => (
+      <a-modal
+        v-model:visible={props.visible}
+        mask-closable={false}
+        body-style={props.bodyStyle}
+        width={computedWidth.value || '500px'}
+        class="modalWrapper"
+        v-bind={attrs}
+        handleOk={handleOk}
+      >
+        {{
+          title: slots.title
+            ? () => (
+                <PrefixTitle v-bind="titleProps">
+                  {slots.default?.()}
+                  {slots.extra?.()}
+                </PrefixTitle>
+              )
+            : undefined,
+          content: () => <></>,
+          footer: () => {
+            slots.footer?.();
+          },
+        }}
+      </a-modal>
+    );
+  },
+});
