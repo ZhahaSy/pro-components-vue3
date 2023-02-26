d44ebd20f5a3fde631f87547b0aab924a3a14c99
ZhahaSy <zhaha_1122@163.com>
2023-02-26 11:58:05 +0800

    新增checkbox组件

---
+++ packages/vue-pro-components/src/Checkbox/index.ts
@@ -0,0 +1,7 @@
+import { withInstall } from '@vue-pro-components/utils';
+// eslint-disable-next-line import/no-self-import
+import Comp from './index';
+
+export const Form = withInstall(Comp);
+
+export default Form;
---
+++ packages/vue-pro-components/src/Checkbox/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div></div>
+</template>
+
+<script setup lang="ts">
+import { OptionProps } from 'ant-design-vue/es/select';
+import { defineProps, reactive } from 'vue';
+
+interface Props {
+  value;
+  options: OptionProps;
+}
+
+defineProps<Props>();
+
+const checked = reactive;
+</script>
